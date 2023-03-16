c8d98d172ce82e97aa4fa4013df28cde00cf0de3
chaimae <cboukili@hotmail.fr>
2023-03-16 06:26:40 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *main - main entry point
+ *return : always 0 (success)
+ */
+int main(void)
+{
+char str1[] = "Programming is like building a multilingual puzzle,";
+char str2[] = " ";
+puts(str1);
+puts(str2);
+return (0);
+}
