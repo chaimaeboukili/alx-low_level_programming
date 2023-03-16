ef9880767f2fd0081f1e7d48a6cb53acb47e0297
chaimae <cboukili@hotmail.fr>
2023-03-16 06:36:17 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 #include <string.h>
 /**
  *main - main entry point
- *return : always 0 (success)
+ * Description: print "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 int main(void)
 {
-char str1[] = "Programming is like building a multilingual puzzle,";
-char str2[] = " ";
+char str1[] = "\"Programming is like building a multilingual puzzle";
 puts(str1);
-puts(str2);
 return (0);
 }
