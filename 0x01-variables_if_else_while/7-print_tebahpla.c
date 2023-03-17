04f0ca996d92bb6fed1162c58b3bb6fdcc5fc437
chaimae <cboukili@hotmail.fr>
2023-03-17 12:41:41 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point prints the lowercase alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
