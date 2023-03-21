8ac474d74fee4bce79919e362710db2fc7441b81
chaimae <cboukili@hotmail.fr>
2023-03-21 09:05:18 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that print 10 times
+ * a string of alphabets
+ * Return: no return value
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	int s = 'a';
+
+	while (i < 10)
+	{
+		while (s <= 'z')
+		{
+			_putchar(s);
+			s++;
+		}
+		s = 'a';
+		i++;
+		_putchar('\n');
+	}
+}
