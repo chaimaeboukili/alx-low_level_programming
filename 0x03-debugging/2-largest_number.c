f2d56ffa449dc8ab059b2096c786a9d6bf78ee5d
chaimae <cboukili@hotmail.fr>
2023-03-23 16:50:06 -0700

    largest number is 972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,23 +15,11 @@ if (a > b && a > c)
 {
 largest = a;
 }
-else if (a > c && c > b)
-{
-largest = a;
-}
-else if (b > a && a > c)
+else if (b > c)
 {
 largest = b;
 }
-else if (b > c && c > a)
-{
-largest = b;
-}
-else if (c > a && a > b)
-{
-largest = c;
-}
-else if (c > b && b > a)
+else
 {
 largest = c;
 }
